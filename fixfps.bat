7b8c2dd89af7aba4fcbcdb0542b829d8c31e0d19
Florin Andrei <florin@andrei.myip.org>
2019-06-16 14:26:19 -0700

    Add files via upload

---
+++ fixfps.bat
@@ -0,0 +1,6 @@
+mp4box -add %1#video -raw 1 -new video
+mp4box -add video_track1.h264:fps=60 -new video_%1
+mp4box -add %1#audio %1.m4a
+ffmpeg -i %1.m4a -filter:a "asetrate=r=48K" -vn audio_%1.m4a
+::ffmpeg -i %1.m4a -af asetrate=24000*2.0,aresample=24000 -vn audio_%1.m4a
+mp4box -add video_%1 -add audio_%1.m4a new_%1
